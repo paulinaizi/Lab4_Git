ac9d385aec48bd5e4bba899607860950793ba4f6
Paulina Izienicka <paulaiz876@gmail.com>
2024-06-06 00:03:27 +0200

    Dodanie opcji --logs

--- lab4.sh
+++ lab4.sh
@@ -6,11 +6,26 @@ print_today()
 	echo "$today"
 }
 
+make_logs()
+{
+	for ((i=1; i<=100; i++));
+	do
+		local file="log${i}.txt"
+		echo $file > $file
+		echo $(basename $BASH_SOURCE) >> $file
+		print_today >> $file
+	done
+}
+
 while [ $# -gt 0 ]
 do
 	case "$1" in
 		--date)
 			print_today
+			shift
+			;;
+		--logs)
+			make_logs
 			shift
 			;;
 		*)
