d64988e72730fb9e1fad5d11235ee61d0b85d06c
Paulina Izienicka <paulaiz876@gmail.com>
2024-06-05 23:42:21 +0200

    Dodanie opcji --date

--- lab4.sh
+++ lab4.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+print_today()
+{
+	today=$(date +"%Y-%m-%d")
+	echo "$today"
+}
+
+while [ $# -gt 0 ]
+do
+	case "$1" in
+		--date)
+			print_today
+			shift
+			;;
+		*)
+			echo "Nieznana opcja"
+			shift
+			;;
+	esac
+done
+
